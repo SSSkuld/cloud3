934099f309e0a9c31acbd3b688680d061b6dbd15
SSSkuld <905361660@qq.com>
2019-01-07 22:00:58 +0800

    前端界面

---
+++ ui/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace cloud3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.createButton = new System.Windows.Forms.Button();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(12, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 105);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "个人空间";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(12, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 106);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "公共空间";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(12, 598);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 91);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "退出云盘";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uploadButton.Location = new System.Drawing.Point(274, 169);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(161, 50);
+            this.uploadButton.TabIndex = 3;
+            this.uploadButton.Text = "上传文件";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.downloadButton.Location = new System.Drawing.Point(333, 664);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(167, 47);
+            this.downloadButton.TabIndex = 4;
+            this.downloadButton.Text = "下载文件";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.deleteButton.Location = new System.Drawing.Point(576, 664);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(159, 47);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "删除文件";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // createButton
+            // 
+            this.createButton.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.createButton.Location = new System.Drawing.Point(441, 169);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(164, 50);
+            this.createButton.TabIndex = 6;
+            this.createButton.Text = "新建文件夹";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // moveButton
+            // 
+            this.moveButton.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.moveButton.Location = new System.Drawing.Point(802, 664);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(227, 47);
+            this.moveButton.TabIndex = 7;
+            this.moveButton.Text = "移动至公共空间";
+            this.moveButton.UseVisualStyleBackColor = true;
+            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(261, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(808, 87);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "欢迎来到cloud3神奇的云盘!!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.pathLabel.Location = new System.Drawing.Point(327, 271);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(716, 31);
+            this.pathLabel.TabIndex = 11;
+            this.pathLabel.Text = "当前路径:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            this.pathLabel.Click += new System.EventHandler(this.pathLabel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 108);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(898, 169);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 50);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "返回上一级";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(12, 474);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(225, 96);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "回收站";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(258, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 31);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "当前路径：";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 761);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ui/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Aliyun.OSS;
+using Aliyun.OSS.Common;
+
+namespace cloud3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lastCount = 0;
+            fileCount = 0;
+            path = "";
+            now_bucket = Config.private_bucket_name;
+            show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //上传
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件夹";
+            dialog.Filter = "所有文件(*.*)|*.*";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = dialog.FileName;
+
+                Upload_file.PutObjectFromFile(now_bucket, file);
+            }
+            show();
+        }
+        private void show()
+        {
+            //显示路径
+            pathLabel.AutoSize = true;
+            pathLabel.Size = new System.Drawing.Size(716, 31);
+            pathLabel.TabIndex = 11;
+            pathLabel.Text = path;
+            pathLabel.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            pathLabel.Location = new System.Drawing.Point(200, 210);
+            
+            //显示文件
+            PictureBox filePicBox;
+            Label fileLable;
+            if (lastCount != 0) //如果上一次没有文件存在 则不用回收上一次显示的
+            {
+                for(int j = 0; j < lastCount; j++)
+                {
+                    this.Controls.Remove(allFilePicBox[j]);
+                    this.Controls.Remove(allFileLabel[j]);
+                }
+            }
+
+            Get_nowpath_allfiles x = new Get_nowpath_allfiles();
+            x.getNowPathAllFiles(now_bucket, path);
+            fileCount = x.list.Count;
+
+            int std_num = 0;
+            for (int j = 0; j < path.Length; j++)
+                if (path[j] == '/')
+                    std_num += 1;
+
+            lastCount = fileCount;
+            int i = 0;
+
+            foreach (FNode s in x.list)
+            {
+                int temp_num = 0;
+                for (int j = 0; j < s.name.Length; j++)
+                    if (s.name[j] == '/')
+                        temp_num += 1;
+                if (s.is_file == false) temp_num -= 1;
+                if (temp_num != std_num)
+                {
+                    continue; // 不符合的跳过不处理
+                }
+
+                //显示图片
+                allFilePicBox[i] = new PictureBox();
+                filePicBox = allFilePicBox[i];
+                if (s.is_file == false)
+                    filePicBox.BackgroundImage = global::cloud3.Properties.Resources.文件夹;
+                else filePicBox.BackgroundImage = global::cloud3.Properties.Resources.文件;
+                filePicBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+                filePicBox.Name = s.name;
+                if (i<=5)
+                    filePicBox.Location= new System.Drawing.Point(200+i*100, 260);
+                else
+                    filePicBox.Location = new System.Drawing.Point(200 + (i-6) * 100, 260+100);
+                filePicBox.Size = new System.Drawing.Size(60, 50);
+                this.Controls.Add(filePicBox);
+
+                filePicBox.Click += new EventHandler(filePicBox_clicked);
+                filePicBox.DoubleClick+= new EventHandler(filePicBox_doubleClicked);
+
+
+                //显示文件名 
+                allFileLabel[i] = new Label();
+                fileLable = allFileLabel[i];
+                fileLable.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+                if(i<=5)
+                    fileLable.Location = new System.Drawing.Point(200 + i * 100, 330);
+                else
+                    fileLable.Location = new System.Drawing.Point(200 + (i - 6) * 100, 330+100);
+                for (int j = s.name.Length - 1; j >= 0; j--)
+                    if (s.name[j] == '/')
+                    {
+                        fileLable.Text = s.name.Substring(j+1);
+                        break;
+                    }
+                this.Controls.Add(fileLable);
+                i++;
+            }
+
+        }
+        private string now_bucket;
+        private string path;   // 当前路径
+        private List<string> fileName = new List<string>();  //要显示的文件名列表
+        private PictureBox[] allFilePicBox = new PictureBox[20];
+        private Label[] allFileLabel = new Label[20];
+        int fileCount; //要显示的文件总数  
+        int lastCount;//上一次显示的文件总数 
+        private List<string> choose = new List<string>();
+        private bool isClicked;
+        private DateTime clickTime;
+
+        private void filePicBox_clicked(object sender, EventArgs e)
+        {
+            PictureBox filePicBox=sender as PictureBox;
+            if (choose.Contains( filePicBox.Name))
+            {
+                choose.Remove(filePicBox.Name);
+                filePicBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else
+            {
+                choose.Add(filePicBox.Name);
+                filePicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            }
+            if (isClicked)
+            {
+                TimeSpan span = DateTime.Now - clickTime;
+                if (span.Milliseconds < SystemInformation.DoubleClickTime)
+                {
+                    filePicBox_doubleClicked(sender, e);
+                    isClicked = false;
+                }
+                else
+                {
+                    isClicked = true;
+                    clickTime = DateTime.Now;
+                }
+            }
+            else
+            {
+                isClicked = true;
+                clickTime = DateTime.Now;
+            }
+
+        }
+        private void filePicBox_doubleClicked(object sender, EventArgs e)
+            //双击文件夹后  转入新的path 
+        {
+            //一顿操作
+            PictureBox filePicBox = sender as PictureBox;
+
+            path = filePicBox.Name;
+
+            show();
+        }
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            //下载
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //删除 
+            int delete = choose.Count;
+            for(int i = 0; i < delete; i++)
+            {
+                fileName.Remove(choose[0]);
+                choose.Remove(choose[0]);
+            }
+            fileCount -= delete;
+            show();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+            //新建文件夹
+        {
+            string str = Interaction.InputBox("请输入新创建空间名称", "创建新空间", "");
+            string folder_path = path + str;
+          
+            folder_path += "/";
+            Create_folder.CreateEmptyFolder(now_bucket, folder_path);
+            show();
+        }
+
+        private void moveButton_Click(object sender, EventArgs e)
+            //移动至公共子空间
+        {
+            int delete = choose.Count;
+            for (int i = 0; i < delete; i++)
+            {
+                fileName.Remove(choose[0]);
+                choose.Remove(choose[0]);
+            }
+            fileCount -= delete;
+            show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            now_bucket = Config.private_bucket_name;
+            path = "";
+            show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            now_bucket = Config.public_bucket_name;
+            path = "";
+            show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pathLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
